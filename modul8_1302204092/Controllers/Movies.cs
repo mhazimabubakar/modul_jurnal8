6f8488ac01ddb6adca112689e63c18e77c0c7452
MUHAMMAD HAZIM ABUBAKAR <mhazimabubakar@365.telkomuniversity.ac.id>
2022-04-23 10:36:35 +0700

    menambahkan controller movies

---
+++ modul8_1302204092/Controllers/Movies.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace modul8_1302204092.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Movies : ControllerBase
+    {
+        // GET: api/<Movies>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<Movies>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<Movies>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        //// PUT api/<Movies>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        // DELETE api/<Movies>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
